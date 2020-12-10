8d6c48851e068fea2d5f0429e528256ad0b064f5
isama22 <emarti17@artic.edu>
2020-12-10 17:27:24 -0600

    very simple navbar on home screen

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,10 +2,14 @@ import React, {Component} from 'react';
 import './Home.css';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import EnterPage from '../../pages/Enterpage/EnterPage';
+import NavBar from '../../components/NavBar/NavBar'
 
 const Home = (props) => {
     return (
         <div className="Home">
+            <div className="navbar-home">
+                <NavBar/>
+            </div>
                 <p>Home page</p>
                
                 <div className="main-image">
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = (props) => {
+    return (
+
+    <div className="nav-container">
+        <span>logged in as: ___</span>
+        <Link>log out</Link>
+    </div>
+    );
+
+}
+export default NavBar;
