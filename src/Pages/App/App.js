cc8cc2beb226e826d43aa3006ff980b9e36d875e
isama22 <emarti17@artic.edu>
2020-12-12 16:09:36 -0600

    moved utils folder inside src

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -3,26 +3,37 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../../Pages/Home/Home'
 import EnterPage from '../../Pages/Enterpage/EnterPage';
 import NavBar from '../../components/NavBar/NavBar'
+import LoginPage from '../../Pages/LoginPage/LoginPage'
+import SignupPage from '../../Pages/SignupPage/SignupPage'
+// import userService from '../../utils/userService';
 
 function App() {
   return (
     <div className="App">
-           <NavBar/>
+      <NavBar />
       <Router>
 
-      {/* <Route path='/' render={() => (
+        {/* <Route path='/' render={() => (
         <Home />
       )} /> */}
-       <Route exact path="/" component={Home} />
-       <Route exact path="/enter" component={EnterPage} />
-      {/* <Route
+        <Route exact path="/" component={Home} />
+        <Route exact path="/enter" component={EnterPage} />
+        {/* <Route
             exact
             path="/enter"
             render={(props) =>
               (<EnterPage {...props} />) 
             }
           /> */}
-
+        <Route exact path='/login' render={() =>
+          <LoginPage
+          />
+        } />
+        <Route exact path='/signup' render={({ history }) =>
+          <SignupPage
+            history={history}
+          />
+        } />
       </Router>
     </div>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,7 +8,6 @@ const NavBar = (props) => {
     <div className="nav-container">
         <span>logged in as: ___</span>
         <Link>log out</Link>
-        {/* <Link to="/enter"> enter </Link> */}
     </div>
     );
 
