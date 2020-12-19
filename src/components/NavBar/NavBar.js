6725528dd267b11a2d1416bcc3b3110862c390c5
isama22 <emarti17@artic.edu>
2020-12-18 18:51:06 -0600

    overflow hiiden on body in index.css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,14 +8,10 @@ const NavBar = (props) => {
         <div className="nav-container">
             <span>welcome, {props.user.name}</span>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            {/* <Link to="/" className="nav-links">home</Link>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
             <Link to="" className="nav-links" onClick={props.handleLogout}>logout</Link>
         </div>
         :
         <div className="nav-container">
-            {/* <Link to="/" className="nav-links">home</Link>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
             <Link to="/signup" className="nav-links">sign up</Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Link to="/login" className="nav-links">log in</Link>
