e9d19ee28cf330df28df983b0dd8c81ca6a611d2
isama22 <emarti17@artic.edu>
2020-12-22 23:45:24 -0600

    added services file for dana lines

---
+++ src/services/danaLines-api.js
@@ -0,0 +1,26 @@
+import tokenService from '../../src/utils/tokenService'
+
+const BASE_URL = '/api/danaLines'
+
+export function getAll() {
+    return fetch(BASE_URL)
+        .then(res => res.json())
+}
+
+export function create(danaLine) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization':  'Bearer ' + tokenService.getToken()
+        },
+        body: JSON.stringify(danaLine)
+    }
+    return fetch(BASE_URL, options).then(res => res.json())
+}
+
+
+export function getOne(danaLine) {
+    return fetch(`${BASE_URL}/${danaLine._id}`).then(res => res.json())
+}
+
