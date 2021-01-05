44c15e1b15b9faffa9f95431c91fc697877dcfb6
isama22 <emarti17@artic.edu>
2021-01-05 17:49:09 -0600

    styled dana page

--- src/Pages/DanaPage/DanaPage.js
+++ src/Pages/DanaPage/DanaPage.js
@@ -17,12 +17,61 @@ async componentDidMount() {
         return (
             <>
                 <div className="dana-page">
-                    <p>dana page</p>
+        
+        <div className="original-text">
 
+        <p className="poem-header">
+              <strong>Don't Let Me Be Wistful</strong>
+              <em>Dana Ward</em>
+            </p>
+        <pre>{`
+            The heat out there this evening
+
+            is contained heat like things keep applicable boxes
+
+            at the ready then subsume their diverging constituent parts
+
+            long ambivalent talks into midnight, all those
+
+            glowy Sophia Coppola movies, Prosecco
+
+            the sexiest records I know. I don’t feel
+
+            how I know anymore to be more than something seeking
+
+            my appalling lack of cinematic knowledge, fictive innocence
+
+            & something like the break-down of those little boxes
+
+            little tombs put their hands up in front of my mouth
+
+            there’s a tacitly humiliating eulogy to drown out couldn’t feel
+
+            much more stupid about feeling stupid over feeling
+
+            stupid, living. Let me go & watch something
+
+            moving several images some of them symbolize humility broken
+
+            postures bloodshot eyes, intimately talking through this
+
+            publicly I am not developing at all but just the same.
+
+            `}
+            <a
+            className="poem-link"
+                href="https://www.poetryfoundation.org/poems/55982/dont-let-me-be-wistful"
+                target="_blank"
+                rel="noopener noreferrer"
+              >...
+            </a>
+            </pre>
                 <DanaLines 
                 className="dana-lines"
                 danaLines={this.state.danaLines}
                 handleGetAllDanaLines={this.props.handleGetAllDanaLines}/>
+            </div>
+
                 </div>
             </>
         );
