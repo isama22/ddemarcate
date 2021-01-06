2f30cc4c40d68719039c86e3f288ff1416e377ea
isama22 <emarti17@artic.edu>
2021-01-05 18:08:31 -0600

    better styling on dana page

--- src/Pages/DanaPage/DanaPage.js
+++ src/Pages/DanaPage/DanaPage.js
@@ -4,73 +4,70 @@ import DanaLines from '../../components/DanaLines/DanaLines'
 import * as danaLinesAPI from '../../services/danaLines-api'
 
 class DanaPage extends Component {
-state = {
-    danaLines: []
+    state = {
+        danaLines: []
 
-}
+    }
 
-async componentDidMount() {
-    const danaLines = await danaLinesAPI.getAll()
-    this.setState({ danaLines })
-}
+    async componentDidMount() {
+        const danaLines = await danaLinesAPI.getAll()
+        this.setState({ danaLines })
+    }
     render() {
         return (
             <>
                 <div className="dana-page">
-        
-        <div className="original-text">
+                    <div className="original-text">
+                        <p className="poem-header">
+                            <strong>Don't Let Me Be Wistful</strong><em>Dana Ward</em>
+                        </p>
+                            <pre>{`
+                                The heat out there this evening
 
-        <p className="poem-header">
-              <strong>Don't Let Me Be Wistful</strong>
-              <em>Dana Ward</em>
-            </p>
-        <pre>{`
-            The heat out there this evening
+                                is contained heat like things keep applicable boxes
 
-            is contained heat like things keep applicable boxes
+                                at the ready then subsume their diverging constituent parts
 
-            at the ready then subsume their diverging constituent parts
+                                long ambivalent talks into midnight, all those
 
-            long ambivalent talks into midnight, all those
+                                glowy Sophia Coppola movies, Prosecco
 
-            glowy Sophia Coppola movies, Prosecco
+                                the sexiest records I know. I don’t feel
 
-            the sexiest records I know. I don’t feel
+                                how I know anymore to be more than something seeking
 
-            how I know anymore to be more than something seeking
+                                my appalling lack of cinematic knowledge, fictive innocence
 
-            my appalling lack of cinematic knowledge, fictive innocence
+                                & something like the break-down of those little boxes
 
-            & something like the break-down of those little boxes
+                                little tombs put their hands up in front of my mouth
 
-            little tombs put their hands up in front of my mouth
+                                there’s a tacitly humiliating eulogy to drown out couldn’t feel
 
-            there’s a tacitly humiliating eulogy to drown out couldn’t feel
+                                much more stupid about feeling stupid over feeling
 
-            much more stupid about feeling stupid over feeling
+                                stupid, living. Let me go & watch something
 
-            stupid, living. Let me go & watch something
+                                moving several images some of them symbolize humility broken
 
-            moving several images some of them symbolize humility broken
+                                postures bloodshot eyes, intimately talking through this
 
-            postures bloodshot eyes, intimately talking through this
-
-            publicly I am not developing at all but just the same.
-
-            `}
-            <a
-            className="poem-link"
-                href="https://www.poetryfoundation.org/poems/55982/dont-let-me-be-wistful"
-                target="_blank"
-                rel="noopener noreferrer"
-              >...
-            </a>
-            </pre>
-                <DanaLines 
-                className="dana-lines"
-                danaLines={this.state.danaLines}
-                handleGetAllDanaLines={this.props.handleGetAllDanaLines}/>
-            </div>
+                                publicly I am not developing at all but just the same.
+                                 `}
+                                <a
+                                    className="poem-link"
+                                    href="https://www.poetryfoundation.org/poems/55982/dont-let-me-be-wistful"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >...
+                                </a>
+                            </pre>
+                        <DanaLines
+                            className="dana-lines"
+                            danaLines={this.state.danaLines}
+                            handleGetAllDanaLines={this.props.handleGetAllDanaLines} 
+                        />
+                    </div>
 
                 </div>
             </>
--- src/components/DanaLine/DanaLine.js
+++ src/components/DanaLine/DanaLine.js
@@ -6,7 +6,7 @@ function DanaLine({ danaLine }) {
     return (
    
         <div className="added-lines">
-            <p>{danaLine.text} <em className="username">username</em></p>
+            <p>{danaLine.text} <em className="username">{danaLine.creator}</em></p>
 
         </div>
 
