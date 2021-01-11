75229f4d66f0f41a41e89dcabb748818783fc39f
isama22 <emarti17@artic.edu>
2021-01-10 18:32:37 -0600

    tweaked styling on a couple pages

--- src/Pages/Enterpage/EnterPage.js
+++ src/Pages/Enterpage/EnterPage.js
@@ -7,8 +7,6 @@ const EnterPage = () => {
   return (
     <div className="enterpage">
       <div className="enter-box">
-
-        {/* <img src="https://i.postimg.cc/wB9KwWDP/output-onlinepngtools-9.png" alt="" className="follow-img"/> */}
         <img src="https://i.postimg.cc/VsW-Sfq2b/Screen-Shot-2020-12-18-at-7-47-18-PM.png" alt="" className="follow-img"/>
         <div className="follow-links">
           <Link className="enter-link" to="/dana1">The heat out there this evening</Link>
