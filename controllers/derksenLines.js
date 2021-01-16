c5f685cae549dc2bbd88f5174ec20b6e04f3bfe4
isama22 <emarti17@artic.edu>
2021-01-16 14:11:50 -0600

    derksen update and delete controller functions

--- controllers/derksenLines.js
+++ controllers/derksenLines.js
@@ -3,7 +3,9 @@ const DerksenLine = require('../models/derksenLine')
 module.exports = {
     index,
     show, 
-    create
+    create,
+    update,
+    delete: deleteOne
 }
 
 async function index(req, res) {
@@ -22,4 +24,19 @@ async function create(req, res) {
     req.body.creator = req.user.name
     const derksenLine = await DerksenLine.create(req.body)
     res.status(201).json(derksenLine)
+}
+
+async function update(req, res) {
+    try {
+        const updatedDerksenLine = await DerksenLine.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json(updatedDerksenLine)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+}
+
+async function deleteOne(req, res) {
+    const deletedDerksenLine = await DerksenLine.findByIdAndRemove(req.params.id)
+    res.status(200).json(deletedDerksenLine)
 }
