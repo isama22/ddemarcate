2dc8fe777ea9de8b86ec3ad4627aa8751f8b81ec
isama22 <emarti17@artic.edu>
2021-01-16 14:17:43 -0600

    derksen update and delete in services file

--- src/services/derksenLines-api.js
+++ src/services/derksenLines-api.js
@@ -19,6 +19,26 @@ export function create(derksenLine) {
     return fetch(BASE_URL, options).then(res => res.json())
 }
 
+export function update(derksenLine) {
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        },
+        body: JSON.stringify(derksenLine)
+    }
+    return fetch(`${BASE_URL}/${erksenaLine._id}`, options).then(res => res.json())
+}
+
 export function getOne(derksenLine) {
     return fetch(`${BASE_URL}/${derksenLine._id}`).then(res => res.json())
+}
+
+export function deleteOne(id) {
+    const options = {
+        method: 'DELETE',
+        headers: {'Authorization': 'Bearer ' + tokenService.getToken()}
+    }
+    return fetch(`${BASE_URL}/${id}`, options).then(res => res.json())
 }
