858899627014e09c327708fd55bfe977df29db0e
isama22 <emarti17@artic.edu>
2021-01-17 16:19:26 -0600

    changed follow a line image on enter to a transparent ascii font

--- src/pages/Enterpage/EnterPage.js
+++ src/pages/Enterpage/EnterPage.js
@@ -7,7 +7,8 @@ const EnterPage = () => {
   return (
     <div className="enterpage">
       <div className="enter-box">
-        <img src="https://i.postimg.cc/VsW-Sfq2b/Screen-Shot-2020-12-18-at-7-47-18-PM.png" alt="" className="follow-img"/>
+        <img src="https://i.postimg.cc/pXQ5ntWF/follow.png" alt="" className="follow-img"/>
+        <img src="https://i.postimg.cc/DZ5yhyBF/output-onlinepngtools-10.png" alt="" className="mobile-follow-img"/>
         <div className="follow-links">
           <Link className="enter-link" to="/dana1">Let me speak with expressive</Link>
           <Link className="enter-link" to="/carson1">I can hear little clicks inside my dream.</Link>
